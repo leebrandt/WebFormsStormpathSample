d1439c61042538bafbc07d32d9d52d5ebefec9c0
Lee Brandt <Lee Brandt>
2016-11-29 11:02:36 -0800

    Initial Commit

---
+++ WebFormsExample/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Stormpath.AspNet;
+
+[assembly: OwinStartup(typeof(WebFormsExample.Startup))]
+
+namespace WebFormsExample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseStormpath();
+        }
+    }
+}
